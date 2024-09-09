cc3c0083821fa252114b3797058d690ec819793c
MargheritaMottana <margherita.mottana@gmail.com>
2024-09-09 16:15:36 +0200

    - importata dalla documentazione di vue la parte di script - creato il messaggio grazie alla text interpolation - aggiunti commenti utili

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script>
+/* 
+export default:
+mi permette di espotare il file, in questo caso il file si chiama App.
+Lo ritrovo collegato in main.js --> "createApp(App)"
+*/
+export default {
+  data() {
+    return {
+      message: 'Hello world'
+    }
+  },
+}
 
 </script>
 
 <template>
+
+  <div>
+    <h1>
+      {{ message }}
+    </h1>
+  </div>
 
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+// App importato dal file App.vue
 import App from './App.vue'
 
 createApp(App).mount('#app')
